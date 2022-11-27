40b084502f9f87d731c667dbb2013f5d64075572
xDekann <oozog255@gmail.com>
2022-11-27 16:59:34 +0100

    Git init

---
+++ aservice/src/main/java/com/aservice/AserviceApplication.java
@@ -0,0 +1,13 @@
+package com.aservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AserviceApplication.class, args);
+	}
+
+}
---
+++ aservice/src/test/java/com/aservice/AserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
