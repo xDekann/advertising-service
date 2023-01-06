2487e31c8203e3d9634a0832ca9a884bef0bf3d2
xDekann <oozog255@gmail.com>
2023-01-06 04:49:31 +0100

    Logout after acc deletion fix/email mod fix + sql

--- aservice/src/main/java/com/aservice/controller/HomeController.java
+++ aservice/src/main/java/com/aservice/controller/HomeController.java
@@ -1,12 +1,30 @@
 package com.aservice.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class HomeController {
+	
 	@GetMapping("/")
 	public String showStartingPage() {
 		return "redirect:/login/start";
 	}
+	
+    @RequestMapping(value="/logout", method=RequestMethod.GET)  
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {  
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();  
+        if (auth != null){      
+           new SecurityContextLogoutHandler().logout(request, response, auth);  
+        }  
+         return "redirect:/";  
+     }  
 }
